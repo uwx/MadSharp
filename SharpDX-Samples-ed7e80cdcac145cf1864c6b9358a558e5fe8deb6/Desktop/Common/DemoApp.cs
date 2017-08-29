2fd3a054ee21022870f64be23d51d2e534612f5c
chrishansen69 <cadrekucra@gmx.com>
2017-08-29 20:46:34 -0300

    yreah

---
+++ SharpDX-Samples-ed7e80cdcac145cf1864c6b9358a558e5fe8deb6/Desktop/Common/DemoApp.cs
@@ -0,0 +1,326 @@
+// Copyright (c) 2010-2013 SharpDX - Alexandre Mutel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// -----------------------------------------------------------------------------
+// Original code from SlimDX project.
+// Greetings to SlimDX Group. Original code published with the following license:
+// -----------------------------------------------------------------------------
+/*
+* Copyright (c) 2007-2011 SlimDX Group
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+using System;
+using System.Windows.Forms;
+using SharpDX.Windows;
+
+namespace SharpDX.Samples
+{
+    /// <summary>
+    /// Root clas for a Demo Application
+    /// </summary>
+    public abstract class DemoApp
+    {
+        private readonly DemoTime _clock = new DemoTime();
+        private FormWindowState _currentFormWindowState;
+        private bool _disposed;
+        private Form _form;
+        private float _frameAccumulator;
+        private int _frameCount;
+
+        /// <summary>
+        ///   Performs object finalization.
+        /// </summary>
+        ~DemoApp()
+        {
+            if (_disposed) return;
+            Dispose(false);
+            _disposed = true;
+        }
+
+        /// <summary>
+        ///   Disposes of object resources.
+        /// </summary>
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                Dispose(true);
+                _disposed = true;
+            }
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        ///   Disposes of object resources.
+        /// </summary>
+        /// <param name = "disposeManagedResources">If true, managed resources should be
+        ///   disposed of in addition to unmanaged resources.</param>
+        protected virtual void Dispose(bool disposeManagedResources)
+        {
+            if (disposeManagedResources)
+            {
+                _form?.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Return the Handle to display to.
+        /// </summary>
+        protected IntPtr DisplayHandle => _form.Handle;
+
+        /// <summary>
+        /// Gets the config.
+        /// </summary>
+        /// <value>The config.</value>
+        public DemoConfiguration Config { get; private set; }
+
+        /// <summary>
+        ///   Gets the number of seconds passed since the last frame.
+        /// </summary>
+        public float FrameDelta { get; private set; }
+
+        /// <summary>
+        ///   Gets the number of seconds passed since the last frame.
+        /// </summary>
+        public float FramePerSecond { get; private set; }
+
+        /// <summary>
+        /// Create Form for this demo.
+        /// </summary>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        protected virtual Form CreateForm(DemoConfiguration config)
+        {
+            return new RenderForm(config.Title)
+            {
+                ClientSize = new System.Drawing.Size(config.Width, config.Height)
+            };
+        }
+
+        /// <summary>
+        /// Runs the demo with default presentation
+        /// </summary>
+        public void Run()
+        {
+            Run(new DemoConfiguration());
+        }
+
+        /// <summary>
+        /// Runs the demo.
+        /// </summary>
+        public void Run(DemoConfiguration demoConfiguration)
+        {
+            Config = demoConfiguration ?? new DemoConfiguration();
+            _form = CreateForm(Config);
+            
+            // disable resizing and center on screen
+            _form.FormBorderStyle = FormBorderStyle.FixedSingle;
+            _form.MaximizeBox = false;
+            _form.StartPosition = FormStartPosition.CenterScreen; 
+            
+            Initialize(Config);
+
+            var isFormClosed = false;
+
+            _form.MouseClick += HandleMouseClick;
+            _form.KeyDown += HandleKeyDown;
+            _form.KeyUp += HandleKeyUp;
+            _form.Resize += (o, args) =>
+            {
+                _currentFormWindowState = _form.WindowState;
+            };
+
+            // ReSharper disable once ImplicitlyCapturedClosure
+            _form.Closed += (o, args) => isFormClosed = true;
+
+            LoadContent();
+
+            _clock.Start();
+            BeginRun();
+            RenderLoop.Run(_form, () =>
+            {
+                if (isFormClosed)
+                {
+                    return;
+                }
+
+                OnUpdate();
+                Render();
+            });
+
+            UnloadContent();
+            EndRun();
+
+            // Dispose explicity
+            Dispose();
+        }
+
+        /// <summary>
+        ///   In a derived class, implements logic to initialize the sample.
+        /// </summary>
+        protected abstract void Initialize(DemoConfiguration demoConfiguration);
+
+        protected virtual void LoadContent()
+        {
+        }
+
+        protected virtual void UnloadContent()
+        {
+        }
+
+        /// <summary>
+        ///   In a derived class, implements logic to update any relevant sample state.
+        /// </summary>
+        protected virtual void Update(DemoTime time)
+        {
+        }
+
+        /// <summary>
+        ///   In a derived class, implements logic to render the sample.
+        /// </summary>
+        protected virtual void Draw(DemoTime time)
+        {
+        }
+
+        protected virtual void BeginRun()
+        {
+        }
+
+        protected virtual void EndRun()
+        {
+        }
+
+        /// <summary>
+        ///   In a derived class, implements logic that should occur before all
+        ///   other rendering.
+        /// </summary>
+        protected virtual void BeginDraw()
+        {
+        }
+
+        /// <summary>
+        ///   In a derived class, implements logic that should occur after all
+        ///   other rendering.
+        /// </summary>
+        protected virtual void EndDraw()
+        {
+        }
+
+        /// <summary>
+        ///   Quits the sample.
+        /// </summary>
+        public void Exit()
+        {
+            _form.Close();
+        }
+
+        /// <summary>
+        ///   Updates sample state.
+        /// </summary>
+        private void OnUpdate()
+        {
+            FrameDelta = (float)_clock.Update();
+            Update(_clock);
+        }
+
+        protected System.Drawing.Size RenderingSize => _form.ClientSize;
+
+        /// <summary>
+        ///   Renders the sample.
+        /// </summary>
+        private void Render()
+        {
+            _frameAccumulator += FrameDelta;
+            ++_frameCount;
+            if (_frameAccumulator >= 1.0f)
+            {
+                FramePerSecond = _frameCount / _frameAccumulator;
+
+                _form.Text = Config.Title + " - FPS: " + FramePerSecond;
+                _frameAccumulator = 0.0f;
+                _frameCount = 0;
+            }
+
+            BeginDraw();
+            Draw(_clock);
+            EndDraw();
+        }
+
+        protected virtual void MouseClick(MouseEventArgs e)
+        {
+        }
+
+        protected virtual void KeyDown(KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                Exit();
+        }
+
+        protected virtual void KeyUp(KeyEventArgs e)
+        {
+        }
+
+        /// <summary>
+        ///   Handles a mouse click event.
+        /// </summary>
+        /// <param name = "sender">The sender.</param>
+        /// <param name = "e">The <see cref = "System.Windows.Forms.MouseEventArgs" /> instance containing the event data.</param>
+        private void HandleMouseClick(object sender, MouseEventArgs e)
+        {
+            MouseClick(e);
+        }
+
+        /// <summary>
+        ///   Handles a key down event.
+        /// </summary>
+        /// <param name = "sender">The sender.</param>
+        /// <param name = "e">The <see cref = "System.Windows.Forms.KeyEventArgs" /> instance containing the event data.</param>
+        private void HandleKeyDown(object sender, KeyEventArgs e)
+        {
+            KeyDown(e);
+        }
+
+        /// <summary>
+        ///   Handles a key up event.
+        /// </summary>
+        /// <param name = "sender">The sender.</param>
+        /// <param name = "e">The <see cref = "System.Windows.Forms.KeyEventArgs" /> instance containing the event data.</param>
+        private void HandleKeyUp(object sender, KeyEventArgs e)
+        {
+            KeyUp(e);
+        }
+    }
+}
